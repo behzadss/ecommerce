f5af6ac99136b5f375b6f5449c1e2f962a950e10
behzas ss <behzadspacey@gmail.com>
2018-07-22 09:21:48 +0430

    linked actions and functions to config.php

--- inc/config.php
+++ inc/config.php
@@ -2,9 +2,13 @@
 $db = mysqli_connect('localhost','behzadss','1757204059','ecommerce');
 if(!$db){
 echo mysqli_connect_error();
-}else{
-echo 'وصل';
 }
 
+
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+
+require_once 'functions.php';
+require_once 'actions.php';
 
 ?>
